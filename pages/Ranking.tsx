7175ab20077c084c02ceddcb87e05f7107b596f9
Paulo <pauloportelamartins@gmail.com>
2022-10-25 09:20:53 -0300

    Created new page and improved visual of current Home
    Now it's fully responsive

---
+++ pages/Ranking.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next';
+import Link from 'next/link';
+
+const Ranking: NextPage = () => {
+  return (
+    <>
+      <footer>
+        <Link href="/">Home</Link>
+      </footer>
+    </>
+  );
+};
+
+export default Ranking;
