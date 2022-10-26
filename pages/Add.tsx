b6fc28edaa89102f616fa8cfdba197eb210a2c2e
Paulo <pauloportelamartins@gmail.com>
2022-10-25 22:12:29 -0300

    Changed styling in add fighter page
    Now "Home" is at the bottom, as intended

--- pages/Add.tsx
+++ pages/Add.tsx
@@ -21,33 +21,35 @@ const Add: NextPage = () => {
   });
 
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-between p-6">
-      <main className="flex flex-col gap-12">
-        <h1 className="text-center font-blackOpsOne text-4xl sm:text-5xl md:text-6xl">
-          Add Fighter
-        </h1>
-
-        <form className="flex flex-col gap-5" onSubmit={handleCreateCharacter}>
-          <label className="flex flex-col gap-1">
-            <span>Name:</span>
-            <input type="text" {...register('name')} className="rounded-sm p-1 text-black" />
-          </label>
-
-          <label className="flex flex-col gap-1">
-            <span>Image *optional:</span>
-            <input name="text" type="text" className="rounded-sm p-1 text-black" />
-          </label>
-
-          <button type="submit" className="mt-2 rounded-md bg-emerald-500 p-2">
+    <>
+      <div className="flex h-screen w-screen flex-col items-center justify-between p-6">
+        <main className="flex flex-col gap-12">
+          <h1 className="text-center font-blackOpsOne text-4xl sm:text-5xl md:text-6xl">
             Add Fighter
-          </button>
-        </form>
-      </main>
-      <footer>
-        <Link href="/">Home</Link>
-      </footer>
+          </h1>
+
+          <form className="flex flex-col gap-5" onSubmit={handleCreateCharacter}>
+            <label className="flex flex-col gap-1">
+              <span>Name:</span>
+              <input type="text" {...register('name')} className="rounded-sm p-1 text-black" />
+            </label>
+
+            <label className="flex flex-col gap-1">
+              <span>Image *optional:</span>
+              <input name="text" type="text" className="rounded-sm p-1 text-black" />
+            </label>
+
+            <button type="submit" className="mt-2 rounded-md bg-emerald-500 p-2">
+              Add Fighter
+            </button>
+          </form>
+        </main>
+        <footer>
+          <Link href="/">Home</Link>
+        </footer>
+      </div>
       <ToastContainer />
-    </div>
+    </>
   );
 };
 
